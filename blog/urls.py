04e54897df497ce7d8c882b56eb5f54560ca5cff
Henry Zhang <zivers@live.cn>
2016-11-22 11:58:05 +0000

    init

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from .import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+]
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#http://yidao620c.github.io/2015/04/21/simpleblog-07.html
+
+python manage.py runserver 0.0.0.0:8080 
